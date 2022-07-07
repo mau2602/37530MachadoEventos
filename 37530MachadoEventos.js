d88a3b11aebdc317a088310d177f201467a9b8ac
Mauro <95579712+mau2602@users.noreply.github.com>
2022-07-07 13:18:34 +0200

    Create 37530MachadoEventos.js

---
+++ 37530MachadoEventos.js
@@ -0,0 +1,22 @@
+var price = 150;
+var helmetAdd = 0;
+
+function show(){
+    let selectBike = document.getElementById("selectBike").value;
+    let weeksEntry = document.getElementById("weeksEntry").value;
+    let helmet = document.getElementById("helmet").value;
+    if (helmet == "si"){
+        helmetAdd = 25;
+    }
+    if (weeksEntry == ""){
+        alert(`Debes seleccionar la duracion del alquiler`)
+    } else {
+        alert(`Has alquilado la moto ${selectBike} por ${weeksEntry} semanas. El precio final es ${(weeksEntry * price) + helmetAdd}.`)
+}
+}
+document.addEventListener("keydown", function(event){
+    const pressedEnter = event.key;
+    if (pressedEnter == 'Enter'){
+        alert(`Para aceptar debes clickear el boton "Seleccionar"`)
+    }
+})
